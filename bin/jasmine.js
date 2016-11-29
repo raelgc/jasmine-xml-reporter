269f49a7bb114376bd534302310eb267a124cf0a
Rael Gugelmin Cunha <rael.gc@gmail.com>
2016-11-29 18:00:39 -0200

    Fixing spec dir to be current dir

--- bin/jasmine.js
+++ bin/jasmine.js
@@ -4,7 +4,7 @@ var path = require('path'),
     Command = require('jasmine/lib/command.js'),
     Jasmine = require('jasmine/lib/jasmine.js');
 
-var jasmine = new Jasmine({ projectBaseDir: path.resolve('jasmine') });
+var jasmine = new Jasmine({ projectBaseDir: path.resolve() });
 var examplesDir = path.resolve('jasmine-core', 'example', 'node_example');
 var command = new Command(path.resolve(), examplesDir, console.log);
 
